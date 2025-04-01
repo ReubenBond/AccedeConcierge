7369d4f8c207f44c77935a2cb9c9858ae6253a9e
Reuben Bond <reuben.bond@gmail.com>
2025-04-01 09:12:31 -0700

    Fix ui visuals

--- src/webui/src/components/App.tsx
+++ src/webui/src/components/App.tsx
@@ -260,23 +260,6 @@ const App: React.FC = () => {
 
     return (
         <div className="app-container">
-            <header className="app-header">
-                <div className="logo">
-                    <img src={logo} alt="Accede Logo" />
-                    <h1>Accede Concierge</h1>
-                </div>
-                <div className="header-actions">
-                    <div className="user-info">
-                        <span className="user-name">Welcome, Terry</span>
-                        <div className="user-avatar">
-                            <svg viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M20 21V19C20 17.9391 19.5786 16.9217 18.8284 16.1716C18.0783 15.4214 17.0609 15 16 15H8C6.93913 15 5.92172 15.4214 5.17157 16.1716C4.42143 16.9217 4 17.9391 4 19V21" stroke="#4A5568" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                <path d="M12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z" stroke="#4A5568" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-            </header>
             <div className="main-content">
                 <div className="chat-window">
                     <ChatContainer
